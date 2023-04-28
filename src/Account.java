3da37bb7d819a1383495b2d57e2a4f5b0aaf5cc0
emresahin101 <ynsmrshn2022@gmail.com>
2023-04-29 01:40:46 +0300

    v1

--- src/Account.java
+++ src/Account.java
@@ -1,5 +1,7 @@
- public class Main {
+ public class Account {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-}
+
+
+     }
--- src/AtmProject.java
+++ src/AtmProject.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class AtmProject {
+public class AtmProject {
+    public static void main(String[] args) {
+        System.out.println("Bu bir ATM projesidir");
+        //version1
+        System.out.println("Java is easy");
+        //version2
+        System.out.println("LEarn Java Earn Money");
+        //version3
+        System.out.println("Git öğrenmek çok kolay;)");
+        //version4
+
+        System.out.println("nesibe branchi kodlarını yazdı.");
+        //version nsb1
+
+        System.out.println("bir kısım kodlar");
+        //nsb2
+
+        System.out.println();
+        //nsb3
+        System.out.println();
+    }
+
 }
